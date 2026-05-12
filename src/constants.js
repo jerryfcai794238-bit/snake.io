b07238da7a3d657dbff2825fc770ef0c58c06a9d
jerryfcai794238-bit <jerryfcai794238@gmail.com>
2026-05-12 13:48:55 +0800

    更新死亡繼承和獲勝比較

--- src/constants.js
+++ src/constants.js
@@ -3,14 +3,14 @@ export const CONFIG = {
     FPS: 60,
     SOLO_TIME: 90,
     HEAD_RADIUS: 15,
-    
+
     // Snake Stats (GDD v2.2.0)
     BASE_SPEED: 4,
     DASH_MULTIPLIER: 1.25, // 下修至提升 25% (v3.1.12)
     INITIAL_LENGTH: 50,
     DASH_LENGTH_CONSUME_RATE: 80, // Length per second (v2.7.2)
     RESPAWN_TIME: 3000, // ms
-    
+
     FOOD_TYPES: {
         SMALL: { value: 2, size: 4 },
         MEDIUM: { value: 5, size: 7 },
